0ba0979c2c1454ccb235a96ada078dd95025e3bf
NotAlexNoyle <alexnoyle@icloud.com>
2025-09-24 19:05:52 -0400

    Update shadow to 8.3.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("java") // Import Java plugin.
     id("java-library") // Import Java Library plugin.
     id("com.diffplug.spotless") version "7.0.4" // Import Spotless plugin.
-    id("com.gradleup.shadow") version "8.3.6" // Import Shadow plugin.
+    id("com.gradleup.shadow") version "8.3.8" // Import Shadow plugin.
     id("checkstyle") // Import Checkstyle plugin.
     eclipse // Import Eclipse plugin.
     kotlin("jvm") version "2.1.21" // Import Kotlin JVM plugin.
